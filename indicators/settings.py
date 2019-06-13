46d1729807611e19f035e5e613c5092913520417
mbanje <mbanjejvernack@gmail.com>
2019-06-13 16:33:25 +0200

    Admin pages with import and export features

--- indicators/settings.py
+++ indicators/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'indicators_app',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- indicators_app/admin.py
+++ indicators_app/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from indicators_app.models import *
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+
+@admin.register(Section)
+class SectionAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(Output)
+class OutputAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(Partner)
+class PartnerAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(Report)
+class ReportAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(Frequency)
+class FrequencyAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(CPD)
+class CPDAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(Indicator)
+class IndicatorAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(InvolvedPartner)
+class InvolvedPartnerAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(ConcernedReport)
+class ConcernedReportAdmin(ImportExportModelAdmin):
+	pass
+
+@admin.register(Measurement)
+class MeasurementAdmin(ImportExportModelAdmin):
+	pass
